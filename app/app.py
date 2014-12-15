ff8e0b644161d3cefb6ca9ac847f5852db3d14d4
Vadim Caen <vadimcaen@gmail.com>
2014-12-15 14:30:28 +0100

    repo structured + login route

--- app/app.py
+++ app/app.py
@@ -7,9 +7,22 @@ def hello_world():
     name = request.args.get('name', '')
     return render_template('index.html', name=name)
 
-@app.errorhandler(404)
+
+@app.route('/login/', methods=['GET', 'POST'] )
+def login():
+    return render_template('auth/signin.html')
+#
+# @app.route('/team/<teamname>/')
+# def team_page(teamname) :
+#     return render_template('team/team.html', teamname=teamname)
+#
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+#
+@app.errorhandler(500)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return "Hey, this is flask the following error has occured : <br/>" + e, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
