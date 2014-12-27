cc5c5f673c26982ed74271a76be891d8b8f03dcc
Anthony Faraut <anthony.faraut@gmail.com>
2014-12-27 21:28:17 +0100

    login and forgetpassword

---
+++ app/mouvinsa/controllers/signin_controller.py
@@ -0,0 +1,6 @@
+__author__ = 'Anthony'
+from wtforms import Form,TextField, PasswordField, validators
+
+class LoginForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.Required()])
