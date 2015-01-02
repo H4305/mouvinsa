7bae4bd55cc607bfd1846fe76cd96abf1a01fe62
nhs1991 <hoangson1991@gmail.com>
2015-01-02 15:31:30 +0100

    Update Confirmation page

--- app/mouvinsa/views.py
+++ app/mouvinsa/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, flash, url_for, redirect
 
 from app import app, db
 from controllers.inscription_controller import InscriptionForm
-from controllers.confirmation_controller import ConfirmationForm
+from controllers.confirmation_controller import ConfirmationForm, updateProfil
 from models import Student, Person, Employee
 from controllers.signin_controller import LoginForm
 from controllers.inscription_controller import createEmployee, createStudent
@@ -43,27 +43,21 @@ def confirmation():
             confirm = request.args.get('msg')
             form = ConfirmationForm(request.form)
             if request.method == "POST":
-                    user_found.lastname = form.lastname.data
-                    user_found.firstname = form.firstname.data
-                    user_found.weight = form.weight.data
-                    user_found.height = form.height.data
-                    user_found.birthdate = form.birthdate.data
+                if form.validate():
+                    updateProfil(form, user_found)
                     db.session.commit()
-                    flash('Vous avez bien enregistré votre profil! Maintenant vous pouvez vous connecter avec votre surnom et votre mot de passe.')
                     return redirect(url_for('login'))
+                else:
+                    return render_template('inscription/confirmation.html', user=user_found, msg='confirme', form=form, form_active=1)
             else:
                 if user_found is not None:
                     if user_found.etat == 'PREREGISTERED':
-                            if confirm == 'confirme':
-                                user_found.etat='REGISTERED'
-                                db.session.commit()
-                                return render_template('inscription/confirmation.html', user=user_found, msg='confirme', form=form)
-                            elif confirm == 'refuse':
-                                user_found.etat='DROPPED'
-                                db.session.commit()
-                                return render_template('inscription/confirmation.html', user=user_found, msg='refuse')
-                    #elif user_found.etat == 'REGISTERED':
-                        #return render_template('inscription/confirmation.html', user=user_found, msg='inscrit')
+                        if confirm == 'confirme':
+                            return render_template('inscription/confirmation.html', user=user_found, msg='confirme', form=form)
+                        elif confirm == 'refuse':
+                            user_found.etat='DROPPED'
+                            db.session.commit()
+                            return render_template('inscription/confirmation.html', user=user_found, msg='refuse')
                 return redirect(url_for('home'))
 
 @app.route('/forgetpassword/', methods=['GET', 'POST'])
@@ -139,8 +133,8 @@ def test_inscription() :
 def list_users() :
     string = ""
     for student in Person.query.all():
-        string += student.__repr__()+unicode(student.lastname)+unicode(student.firstname)\
-                  +unicode(student.birthdate)+student.etat
+        string += student.__repr__()+': lastname-'+unicode(student.lastname)+', firstname-'+unicode(student.firstname)\
+                  +', birthdate-'+unicode(student.birthdate)+', etat-'+student.etat+', sex-'+student.sex
     return string
 
 @app.route('/test/confirmation')
@@ -151,15 +145,18 @@ def test_confirmation() :
         db.session.commit()
     student2 = Student()
     student2.nickname = 'test4'
+    student2.firstname=''
+    student2.lastname=''
     student2.password = 'password'
     student2.email = 'email4@email.com'
     student2.category = 'etudiant'
     student2.etat = 'preregistered'
-    student2.sex = 'Masculin'
+    student2.sex = ''
     student2.token = 'a0114'
     db.session.add(student2)
     db.session.commit()
     string = ""
     for student in Person.query.all():
-        string += student.__repr__()+student.token+student.etat
+        string += student.__repr__()+': lastname-'+unicode(student.lastname)+', firstname-'+unicode(student.firstname)\
+                  +', birthdate-'+unicode(student.birthdate)+', etat-'+student.etat+', token-'+student.token+', sex-'+student.sex
     return string
