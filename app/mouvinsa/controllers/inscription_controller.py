c441b275d9ae8838a77c762148a739bd86ae6d52
ldanilescu <ludmila.danilescu@insa-lyon.fr>
2015-01-14 10:38:37 +0100

    model etudiant (changement type pour year, branch, cycle)

--- app/mouvinsa/controllers/inscription_controller.py
+++ app/mouvinsa/controllers/inscription_controller.py
@@ -8,46 +8,40 @@ def hash_password(password):
     return sha256(salt.encode() + password.encode()).hexdigest() + \
         ':' + salt
 
+
+messageObligatoire='Ce champs est obligatoire. Veuillez le remplir.'
+messageEmail='Les 2 emails doivent correspondre. Veuillez reessayer.'
+messageLongueur2_25='La longeur doit etre comprise entre 2 et 25 caracteres.'
+messagePassword='Les 2 mots de passe doivent correspondre.  Veuillez reessayer.'
+messageLongueur4_25='La longeur doit etre comprise entre 4 et 25 caracteres.'
+messagePoids='Le poids doit etre compris entre 20 kg et 300 kg'
+messageTaille='La taille doit etre comprise entre 90 cm et 250 cm.'
+messageLongueur3_100='La longeur doit etre comprise entre 3 et 100 caracteres.'
+choices=[('', ''), ('Premier', 'Premier'), ('Second', 'Second')]
+
 class InscriptionForm(Form):
-	email = TextField(u'Email', [validators.Required(message='Ce champs est obligatoire. Veuillez le remplir.'), validators.EqualTo('confirmEmail', message='Les 2 emails doivent correspondre. Veuillez reessayer.')])
-
-	confirmEmail = TextField(u'Confirmez votre email', [validators.Required(message='Ce champs est obligatoire. Veuillez le remplir.')])
-
-	surnom = TextField(u'Pseudonyme', [validators.Required(message='Ce champs est obligatoire. Veuillez le remplir.'), validators.Length(min=2, max=25, message='La longeur doit etre comprise entre 2 et 25 caracteres.')])
-
-	nom = TextField(u'Nom', [validators.Optional(), validators.Length(min=2, max=25, message='La longeur doit etre comprise entre 2 et 25 caracteres.')])
-
-	prenom = TextField(u'Prenom', [validators.Optional(), validators.Length(min=2, max=25, message='La longeur doit etre comprise entre 2 et 25 caracteres.')])
-
-	categorie = SelectField(u'Categorie', [validators.Required(message='Ce champs est obligatoire. Veuillez choisir un item dans la liste.')], choices=[('Etudiant', 'Etudiant'), ('Enseignant-Chercheur', 'Enseignant-Chercheur'), ('Personnel BIATOS', 'Personnel BIATOS')])
-
+	email = TextField(u'Email', [validators.Required(message=messageObligatoire), validators.EqualTo('confirmEmail', message=messageEmail)])
+	confirmEmail = TextField(u'Confirmez votre email', [validators.Required(message=messageObligatoire)])
+	surnom = TextField(u'Pseudonyme', [validators.Required(message=messageObligatoire), validators.Length(min=2, max=25, message=messageLongueur2_25)])
+	nom = TextField(u'Nom', [validators.Optional(), validators.Length(min=2, max=25, message=messageLongueur2_25)])
+	prenom = TextField(u'Prenom', [validators.Optional(), validators.Length(min=2, max=25, message=messageLongueur2_25)])
+	categorie = SelectField(u'Categorie', [validators.Required(message=messageObligatoire)], choices=[('Etudiant', 'Etudiant'), ('Enseignant-Chercheur', 'Enseignant-Chercheur'), ('Personnel BIATOS', 'Personnel BIATOS')])
 	annee = SelectField(u'Annee', choices=[('', ''), ('Premiere', 'Premiere'), ('Deuxieme', 'Deuxieme'), ('Troisieme', 'Troisieme'), ('Quatrieme', 'Quatrieme'), ('Cinquieme', 'Cinquieme')])
-
 	cycle = SelectField(u'Cycle', choices=[('', ''), ('Premier', 'Premier'), ('Second', 'Second')])
-
 	filiere = SelectField(u'Filiere', choices=[('', ''), ('Filiere Internationale','Filiere Internationale'), ('Filiere Clasique', 'Filiere Clasique'), ('PCE','PCE'), ('FAS','FAS'), ('SHN','SHN')])
-
 	departement = SelectField(u'Departement', choices=[('',''), ('BB', 'BB'), ('BIM', 'BIM'), ('GE', 'GE'), ('GI', 'GI'), ('GCU', 'GCU'), ('GEN', 'GEN'), ('GMC', 'GMC'), ('GMD', 'GMD'), ('GMPP', 'GMPP'), ('IF', 'IF'), ('SGM', 'SGM'), ('TC', 'TC')])
-
 	password = PasswordField(u'Mot de Passe', [
-		validators.Required(message='Ce champs est obligatoire. Veuillez le remplir.'),
-		validators.EqualTo('confirm', message='Les 2 mots de passe doivent correspondre.  Veuillez reessayer.'),
-		validators.Length(min=4, max=25, message='La longeur doit etre comprise entre 4 et 25 caracteres.')
+		validators.Required(message=messageObligatoire),
+		validators.EqualTo('confirm', message=messagePassword),
+		validators.Length(min=4, max=25, message=messageLongueur4_25)
 	])
-
-	confirm = PasswordField(u'Confirmez le mot de passe', [validators.Required(message='Ce champs est obligatoire. Veuillez le remplir.')])
-
+	confirm = PasswordField(u'Confirmez le mot de passe', [validators.Required(message=messageObligatoire)])
 	dateNaissance = DateField(u'Ne(e) le', format='%d/%m/%Y',  validators=[validators.Optional()])
-
 	sexe = SelectField(u'Sexe ', choices=[('', ''), ('Masculin', 'Masculin'), ('Feminin', 'Feminin')])
-
-	poids = FloatField(u'Poids (kg)', [validators.Optional(), validators.NumberRange(min=20, max=300, message='Le poids doit etre compris entre 20 kg et 300 kg')])
-
-	hauteur = FloatField(u'Taille (cm)', [validators.Optional(), validators.NumberRange(min=90, max=250, message='La taille doit etre comprise entre 90 cm et 250 cm.')])
-
-	position = TextField(u'Position', [validators.Optional(), validators.Length(min=2, max=100,  message='La longeur doit etre comprise entre 3 et 100 caracteres.')])
-
-	affiliation = TextField(u'Affiliation', [validators.Optional(), validators.Length(min=2, max=100,  message='La longeur doit etre comprise entre 3 et 100 caracteres.')])
+	poids = FloatField(u'Poids (kg)', [validators.Optional(), validators.NumberRange(min=20, max=300, message=messagePoids)])
+	hauteur = FloatField(u'Taille (cm)', [validators.Optional(), validators.NumberRange(min=90, max=250, message=messageTaille)])
+	position = TextField(u'Position', [validators.Optional(), validators.Length(min=2, max=100,  message=messageLongueur3_100)])
+	affiliation = TextField(u'Affiliation', [validators.Optional(), validators.Length(min=2, max=100,  message=messageLongueur3_100)])
 
 
 def createStudent(form, student):
@@ -61,7 +55,7 @@ def createStudent(form, student):
 	else:
 		student.sex = form.sexe.data
 	student.category = 'etudiant'
-	student.year = form.annee.data
+	student.year = 'premiere'#form.annee.data
 	student.cycle = form.cycle.data
 	if form.cycle.data == 'Premier':
 		student.branch = form.filiere.data
