915a67b6cb367876e6324ccac07b573916e2f237
Bernardo Rittmeyer <bernardo@rittme.com>
2015-01-14 18:13:11 +0100

    NEW VAGRANT MACHINE WITHOUT UWSGI. logs on env/flask.log

--- app/runserver.py
+++ app/runserver.py
@@ -1,6 +1,7 @@
 __author__ = 'vcaen'
 from mouvinsa.app import app
-
+log = open("/vagrant/flask.log", "w")
+log.close()
 
 if __name__=='__main__':
-    app.run('127.0.0.1',debug=True)
+    app.run(host='0.0.0.0',port=8080,debug=True)
