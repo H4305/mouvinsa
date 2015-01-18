1cf632be4f7b574b919b98417113014723cef255
Vadim Caen <vadimcaen@gmail.com>
2015-01-18 20:26:12 +0100

    Finished Models

--- app/mouvinsa/models.py
+++ app/mouvinsa/models.py
@@ -4,10 +4,20 @@ from sqlalchemy.orm import backref
 from mouvinsa.app import db
 
 
+# Defining Tables for n-n relationship
+badges_person = db.Table('badges_person',
+                         db.Column('person_id', db.Integer, db.ForeignKey('person.id')),
+                         db.Column('badge_id', db.Integer, db.ForeignKey('badges.id'))
+)
+city_goal = db.Table('city_goal',
+                     db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
+                     db.Column('goal_id', db.Integer, db.ForeignKey('goal.id'))
+)
+
+# Defining models
 class Person(db.Model):
     __tablename__ = 'person'
     id = db.Column(db.Integer, primary_key=True)
-    # idGroup = db.Column(db.Integer)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     nickname = db.Column(db.String(120), unique=True, nullable=False)
@@ -21,7 +31,12 @@ class Person(db.Model):
     type = db.Column(db.String(50))
     etat = db.Column('etat', Enum('PREREGISTERED', 'REGISTERED', 'DROPPED'), nullable=False)
     token = db.Column(db.String(128), unique=True)
-    group_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    steps = db.relationship('Steps', backref='person', lazy='dynamic')
+    fitnessInfo = db.relationship('FitnessInfo', backref='person', lazy='dynamic')
+    badges = db.relationship('Badge',
+                             secondary=badges_person,
+                             backref=db.backref('person', lazy='dynamic'))
 
     def __init__(self):
         print "init person"
@@ -64,7 +79,16 @@ class Goal(db.Model):
     label = db.Column(db.String(45), nullable=False)
     distance = db.Column(db.Integer, nullable=False)
     image = db.Column(db.String(45), nullable=False)
-    groups = db.relationship('Group', backref='goals', )
+    groups = db.relationship('Group', backref='goals', lazy='dynamic')
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(127), nullable=False)
+    lattitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    caption = db.Column(db.String(255))
+    description = db.Column(db.Text)
 
 
 class Group(db.Model):
@@ -75,16 +99,32 @@ class Group(db.Model):
     stepSum = db.Column(db.Integer, default=0)
     image = db.Column(db.String(255), nullable=True)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
-    persons = db.relationship('Person', backref='group',
-                            lazy='dynamic')
+    persons = db.relationship('Person', backref='group', lazy='dynamic')
+
 
 class Steps(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     date = db.Column(db.DateTime)
+    stepnumber = db.Column(db.Integer, default=0)
 
 
+class FitnessInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    stepSum = db.Column(db.Integer, default=0, nullable=True)
+    streak = db.Column(db.Integer, default=0)
+    bestStreak = db.Column(db.Integer, default=0)
+    goal = db.Column(db.Integer, default=0)
 
 
+class Badge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(127), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    image = db.Column(db.String(255), nullable=True)
 
 
+class Vip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=True)
