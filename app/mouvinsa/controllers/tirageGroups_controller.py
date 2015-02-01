a6feabe3c4420d9073a6a42a2a72cd408f5e7a88
ldanilescu <ludmila.danilescu@insa-lyon.fr>
2015-02-01 13:30:34 +0100

    noms equipes

--- app/mouvinsa/controllers/tirageGroups_controller.py
+++ app/mouvinsa/controllers/tirageGroups_controller.py
@@ -28,4 +28,11 @@ def tirageGroups():
         index = index + 1
         person.group_id = index
         if index == 41:
-            index=34
+            index=18
+
+nomsGroupes = [ u"Jumperzap", u"Highjumps", u"Batsqueak", u"Athletebrring", u"Sportouch", u"Catcherparp", u"Frisbeeplonk",
+                u"Skateroar", u"Tiecheep", u"Tennisbuzz", u"Volleytweet", u"Gearmeow", u"Outslouch", u"Hockeytwang",
+                u"Rowingthump", u"Uniformfizz", u"Walkgrowl", u"Hardballping", u"Freethrowrip", u"Golfingsmash", u"Championouch",
+                u"Hockeyhoot", u"Lutzboink", u"Fitnesstweet", u"Bowlingbelch", u"Runningshush", u"Surferfizz", u"Batbeep",
+                u"Biathlonboom", u"Battingbuzz", u"Cyclingcuckoo", u"Dartdingdong", u"Divedrip", u"Fitnessfizzle", u"Footballfizz",
+                u"Boxingboom", u"Movementbuzz", u"Waterskiwoof", u"Canoeingcheep", u"Crazy Legs", u"Catchmurmer"]
