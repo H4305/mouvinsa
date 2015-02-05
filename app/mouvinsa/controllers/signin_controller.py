ef9b8fa949bfa48fe690f51b4656e15c06dccdb2
Anthony Faraut <anthony.faraut@gmail.com>
2015-02-05 15:21:28 +0100

    avancement de signin

--- app/mouvinsa/controllers/signin_controller.py
+++ app/mouvinsa/controllers/signin_controller.py
@@ -3,5 +3,10 @@
 from wtforms import Form,TextField, PasswordField, validators
 
 class LoginForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email', [validators.Length(min=4, max=25)])
     password = PasswordField('Password', [validators.Required()])
+
+
+
+class MdpForm(Form):
+    email = TextField('Email', [validators.Length(min=4, max=25)])
