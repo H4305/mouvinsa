fa89ff921da894e1fce63d67ff2df0f16380ff8f
Marco Montalto <marco.montalto@insa-lyon.fr>
2015-02-06 21:55:06 +0100

    ignore bdd dumps into BDBackup

--- app/mouvinsa/models.py
+++ app/mouvinsa/models.py
@@ -9,16 +9,26 @@ badges_person = db.Table('badges_person',
                          db.Column('person_id', db.Integer, db.ForeignKey('person.id')),
                          db.Column('badge_id', db.Integer, db.ForeignKey('badge.id'))
 )
-city_goal = db.Table('city_goal',
+
+"""city_goal = db.Table('city_goal',
                      db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
                      db.Column('goal_id', db.Integer, db.ForeignKey('goal.id'))
-)
+)"""
 
 person_steps = db.Table('person_steps',
                      db.Column('person_id', db.Integer, db.ForeignKey('person.id')),
                      db.Column('step_id', db.Integer, db.ForeignKey('steps.id'))
 )
 
+group_goals = db.Table('group_goals',
+                      db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
+                      db.Column('city_tres_facile_id', db.Integer, db.ForeignKey('city.id')),
+                      db.Column('city_facile_id', db.Integer, db.ForeignKey('city.id')),
+                      db.Column('city_moyen_id', db.Integer, db.ForeignKey('city.id')),
+                      db.Column('city_difficile_id', db.Integer, db.ForeignKey('city.id')),
+                      db.Column('city_tres_difficile_id', db.Integer, db.ForeignKey('city.id')),
+                      db.Column('city_champion_id', db.Integer, db.ForeignKey('city.id'))
+)
 
 # Defining models
 class Person(db.Model):
@@ -69,7 +79,6 @@ class Student(Person):
         'polymorphic_identity': 'student',
     }
 
-
 class Employee(Person):
     __tablename__ = 'employee'
     id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
@@ -80,24 +89,47 @@ class Employee(Person):
         'polymorphic_identity': 'employee',
     }
 
-
-class Goal(db.Model):
-    __tablename__ = 'goal'
+class Level(db.Model):
+    __tablename__ = 'level'
     id = db.Column(db.Integer, primary_key=True)
-    difficulty = db.Column('difficulty', Enum('difficile', 'moyen', 'facile'), nullable=False)
-    label = db.Column(db.String(45), nullable=False)
-    distance = db.Column(db.Integer, nullable=False)
-    image = db.Column(db.String(45), nullable=False)
-    groups = db.relationship('Group', backref='goals', lazy='dynamic')
+    label = db.Column(db.String(127), nullable=False)
+    distance = db.Column(db.Integer, nullable=False, unique=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'level',
+    }
+
+    cities = db.relationship('City', backref='level', lazy='dynamic')
 
 
 class City(db.Model):
+    __tablename__ = 'city'
     id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(127), nullable=False)
-    lattitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    caption = db.Column(db.String(255))
+    nom = db.Column(db.String(127), nullable=False)
     description = db.Column(db.Text)
+    image_atteinte1 = db.Column(db.String(120), nullable=True)
+    image_atteinte2 = db.Column(db.String(120), nullable=True)
+    image_atteinte3 = db.Column(db.String(120), nullable=True)
+    image_but = db.Column(db.String(120), nullable=True)
+    #lattitude = db.Column(db.Float, nullable=False)
+    #longitude = db.Column(db.Float, nullable=False)
+    #caption = db.Column(db.String(255))
+    level_id = db.Column(db.Integer, db.ForeignKey('level.id'))
+
+    groups = db.relationship('Group', backref='cities', lazy='dynamic')
+    __mapper_args__ = {
+        'polymorphic_identity': 'city',
+    }
+
+
+"""class Goal(db.Model):
+    __tablename__ = 'goal'
+    id = db.Column(db.Integer, primary_key=True)
+    #difficulty = db.Column('difficulty', Enum('difficile', 'moyen', 'facile'), nullable=False)
+    #label = db.Column(db.String(45), nullable=False)
+    #distance = db.Column(db.Integer, nullable=False)
+    #image = db.Column(db.String(45), nullable=False)
+    #groups = db.relationship('Group', backref='goals', lazy='dynamic')"""
 
 
 class Group(db.Model):
@@ -107,11 +139,18 @@ class Group(db.Model):
     slogan = db.Column(db.String(255), nullable=True)
     stepSum = db.Column(db.Integer, default=0)
     image = db.Column(db.String(255), nullable=True)
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    city_arrived_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+    city_destination_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'group',
+    }
+
     persons = db.relationship('Person', backref='group', lazy='dynamic')
 
 
 class Steps(db.Model):
+    __tablename__ = 'steps'
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     date = db.Column(db.DateTime)
@@ -119,6 +158,7 @@ class Steps(db.Model):
 
 
 class FitnessInfo(db.Model):
+    __tablename__ = 'fitnessInfo'
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     stepSum = db.Column(db.Integer, default=0, nullable=True)
@@ -128,12 +168,13 @@ class FitnessInfo(db.Model):
 
 
 class Badge(db.Model):
+    __tablename__ = 'badge'
     id = db.Column(db.Integer, primary_key=True)
     label = db.Column(db.String(127), nullable=False)
     description = db.Column(db.String(255), nullable=True)
     image = db.Column(db.String(255), nullable=True)
 
 
-class Vip(db.Model):
+"""class Vip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), nullable=True)
+    email = db.Column(db.String(255), nullable=True)"""
