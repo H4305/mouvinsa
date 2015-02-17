a8bf18ebf4968c9a873c7298b936e682a5d4684e
Bernardo Rittmeyer <bernardo@rittme.com>
2015-02-17 19:14:38 +0100

    config file configuring configurations configurated

--- app/mouvinsa/config.py
+++ app/mouvinsa/config.py
@@ -30,3 +30,11 @@ SECRET_KEY = "secret"
 
 # administrator list
 ADMIN = ['mouvinsa.communication@gmail.com']
+
+DEBUG = True
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'mouvinsa.communication@gmail.com'
+MAIL_PASSWORD = '**********'
