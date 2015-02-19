9e7e323ef272ca53682a6102b77892cee47cd801
Vadim Caen <vadimcaen@gmail.com>
2015-02-19 19:33:22 +0100

    added debugger mode on port 8081 with 'debug' args in command line

--- app/runserver.py
+++ app/runserver.py
@@ -1,7 +1,12 @@
+import sys
+
 __author__ = 'vcaen'
 from mouvinsa.app import app
 #log = open("/vagrant/flask.log", "w")
 #log.close()
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    port = 8080
+    if sys.argv[1] == 'debug':
+        port = 8081
+    app.run(host='0.0.0.0',port=port,debug=True)
