b4cbfe54230a70e97ed30b255e4064db8fbab36e
Vadim Caen <vadimcaen@gmail.com>
2015-02-26 17:39:17 +0100

    finish slideshow animation

--- app/mouvinsa/static/scripts/group/infobox.js
+++ app/mouvinsa/static/scripts/group/infobox.js
@@ -2,9 +2,15 @@
  * Created by vcaen on 25/02/2015.
  */
 
-slidshow = function() {
-    $(".slideshow ul").animate({marginLeft: -590}, 700, function () {
+function slidshow() {
+    $(".slideshow ul").delay(3000).animate({marginLeft: -590}, 900, 'swing', function () {
         $(this).css({marginLeft: 0}).find("li:last").after($(this).find("li:first"));
         slidshow();
     });
-}
+};
+
+slidshow();
+
+
+
+
