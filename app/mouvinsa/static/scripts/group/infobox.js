cff5893d78f5527c81a105520da62bff5ec76f39
Vadim Caen <vadimcaen@gmail.com>
2015-02-26 09:13:45 +0100

    fixed postion

--- app/mouvinsa/static/scripts/group/infobox.js
+++ app/mouvinsa/static/scripts/group/infobox.js
@@ -1,3 +1,10 @@
 /**
  * Created by vcaen on 25/02/2015.
  */
+ $(function(){
+      setInterval(function(){
+         $(".slideshow ul").animate({marginLeft:-350},800,function(){
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 3500);
+   });
