eca813b40f1bbeffcf90c2e70603d8dd28b13af7
Marco Montalto <marco.montalto@insa-lyon.fr>
2015-02-26 16:38:17 +0100

    round distance km index

--- app/mouvinsa/views.py
+++ app/mouvinsa/views.py
@@ -58,7 +58,7 @@ def home():
         nbPasTotales = nbPasTotales + group.stepSum
         distanceTotale = (distanceTotale + group.distance)
 
-    distanceTotale = distanceTotale/1000
+    distanceTotale = round(distanceTotale/1000, 1)
     toursTerre = round(distanceTotale/40075, 2)
     return render_template('/accueil/index.html', person=person, index=index, groups=groups, nbPasTotales="{:,}".format(nbPasTotales), distanceTotale="{:,}".format(distanceTotale), toursTerre=toursTerre)
 
