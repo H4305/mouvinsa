4aa79f73538988a6b7343e2c27e0a8dd166b2dea
Vadim Caen <vadimcaen@gmail.com>
2015-02-26 13:44:32 +0100

    slideshow working

--- app/mouvinsa/static/scripts/group/infobox.js
+++ app/mouvinsa/static/scripts/group/infobox.js
@@ -1,10 +1,10 @@
 /**
  * Created by vcaen on 25/02/2015.
  */
- $(function(){
-      setInterval(function(){
-         $(".slideshow ul").animate({marginLeft:-350},800,function(){
-            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-         })
-      }, 3500);
-   });
+
+slidshow = function() {
+    $(".slideshow ul").animate({marginLeft: -590}, 700, function () {
+        $(this).css({marginLeft: 0}).find("li:last").after($(this).find("li:first"));
+        slidshow();
+    });
+}
