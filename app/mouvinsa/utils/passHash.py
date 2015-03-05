c6863f1a50e54b9620acb0613254991463d6bc77
Vadim Caen <vadimcaen@gmail.com>
2015-03-05 20:16:17 +0100

    fix bug password with special characters

--- app/mouvinsa/utils/passHash.py
+++ app/mouvinsa/utils/passHash.py
@@ -3,10 +3,9 @@ import hashlib
 
 def hash_password(password):
     salt = uuid.uuid4().hex
-    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + \
-        ':' + salt
+    return hashlib.sha256(salt.encode() + password.encode('utf8')).hexdigest() +  ':' + salt
 
 def check_password(hashed_password, user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() +
-                                      user_password.encode()).hexdigest()
+                                      user_password.encode('utf8')).hexdigest()
