4189e4462fe90dcfa860c8ca5aee8364bb98f02d
Vadim Caen <vadimcaen@gmail.com>
2015-03-05 19:29:28 +0100

    corrected max-width on main previously commented

--- app/mouvinsa/static/scripts/paralax.js
+++ app/mouvinsa/static/scripts/paralax.js
@@ -4,11 +4,15 @@
 (function() {
 
     var header = $('header');
+    var avatar = $('.person-header > .container');
 
     $(window).scroll(function () {
         s = $(document).scrollTop();
        
         $(header).css("-webkit-transform", "translateY(" + (s * 0.4) + "px)");
         $(header).css("transform", "translateY(" + (s * 0.4) + "px)");
+        $(avatar).css("-webkit-transform", "translateY(" + (-s * 0.9) + "px)");
+        $(avatar).css("transform", "translateY(" + (-s * 0.9) + "px)");
+
     });
 })();
